aceadfb2b04cee2c658056fc700dded8c661c76c
Yohannes <yohannes.gobezie@gmail.com>
2023-12-20 15:17:07 -0700

    First commit

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,34 @@
+// react router dom (rrd) imports
+// allows us to access whatever is in the loader function
+import { Outlet, useLoaderData } from "react-router-dom";
+
+// assets
+import wave from "../assets/wave.svg";
+
+// helper functions
+import { fetchData } from "../helpers"
+
+// components
+import Nav from "../components/Nav";
+
+// loader
+export function mainLoader() {
+    const userName = fetchData("userName");
+    return { userName }
+}
+
+const Main = () => {
+    const {userName} = useLoaderData()
+
+    return (
+        <div className="layout">
+            <Nav userName={userName}/>
+            <main>
+                <Outlet />
+            </main>
+            <img src={wave} alt="" />
+        </div>
+    )
+}
+
+export default Main
